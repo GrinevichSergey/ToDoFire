8b0a7a4f739b308b7a0c32995cd307173acc4f62
Sergey <grinevich95@bk.ru>
2019-04-08 01:18:30 +0300

    authuser

---
+++ ToDoFire/ToDoFire/controller/RegisterViewController.swift
@@ -0,0 +1,101 @@
+//
+//  RegisterViewController.swift
+//  ToDoFire
+//
+//  Created by Сергей Гриневич on 07/04/2019.
+//  Copyright © 2019 Green. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    func warningLabelText(warningLAbel text: String )
+    {
+        warnLabel.text = text
+        UIView.animate(withDuration: 3, delay: 0, options: [.curveEaseInOut], animations: { [weak self] in
+            self?.warnLabel.alpha = 1
+        }) { [weak self] (complete) in
+            self?.warnLabel.alpha = 0
+        }
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+        guard let email = emailText.text, let password = passwordText.text, email != "", password != "" else {
+            warningLabelText(warningLAbel: "Info is incorrect")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] (user, error) in
+            if error != nil
+            {
+                self?.warningLabelText(warningLAbel: "Error ocurred")
+                return
+            }
+            
+            if user != nil {
+                self?.performSegue(withIdentifier: "taskssegue", sender: nil)
+                return
+            }
+            
+            self?.warningLabelText(warningLAbel: "No user")
+            
+        }
+        
+    }
+    @IBAction func registerTapped(_ sender: UIButton) {
+        
+        guard let email = emailText.text, let password = passwordText.text, email != "", password != "" else {
+            warningLabelText(warningLAbel: "Info is incorrect")
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self]  (user, error) in
+            if error == nil
+            {
+                if user != nil
+                {
+                     self?.performSegue(withIdentifier: "taskssegue", sender: nil)
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //скрыть клавиатуру
+        NotificationCenter.default.addObserver(self, selector: #selector(RegisterViewController.keyboardDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(RegisterViewController.keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+
+        warnLabel.alpha = 0
+    }
+    
+    //клава
+    @objc func keyboardDidShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+//            if self.view.frame.origin.y == 0{
+//                self.view.frame.origin.y -= keyboardSize.height
+//            }
+            (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + keyboardSize.height )
+            (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height , right: 0)
+        }
+        
+    }
+    //клава
+    @objc func keyboardDidHide(notification: Notification) {
+
+             (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height )
+            
+        }
+    
+    
+    
+}
