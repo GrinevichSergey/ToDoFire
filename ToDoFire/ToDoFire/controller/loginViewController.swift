3823209da776593674d96dcc6b682c4694eebc7e
Sergey <grinevich95@bk.ru>
2019-04-07 23:32:54 +0300

    f

--- ToDoFire/ToDoFire/controller/loginViewController.swift
+++ ToDoFire/ToDoFire/controller/loginViewController.swift
@@ -1,33 +1,19 @@
 //
-//  ViewController.swift
-//  ToDoFire
+//  loginViewController.swift
+//  
 //
-//  Created by Сергей Гриневич on 06/04/2019.
-//  Copyright © 2019 Green. All rights reserved.
+//  Created by Сергей Гриневич on 07/04/2019.
 //
 
 import UIKit
 
-
 class loginViewController: UIViewController {
 
-    @IBOutlet weak var emailLabel: UITextField!
-    @IBOutlet weak var passwordLabel: UITextField!
-    @IBOutlet weak var warnLabel: UILabel!
-    @IBOutlet weak var registerButton: UIButton!
-    @IBOutlet weak var loginLabel: UIButton!
-    
-    @IBAction func registerButtonPresed(_ sender: UIButton) {
-    }
-    @IBAction func loginButtonPresed(_ sender: UIButton) {
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
 
+        // Do any additional setup after loading the view.
     }
 
 
 }
-
